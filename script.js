552271b42709359274e9895877c3bb571e00c7dd
Will Brown <williambrown@williams-air.lan>
2019-07-17 23:52:02 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var shoutouts = [
+  "You are so nice and easy to talk to.",
+"Obvi ellie but I love how you make everyday very positive and how you are always asking questions (like how to work the coffee machine)",
+"you are super positive and ellie is the best!",
+"your questions! Even when no one else asks the question, you make it so much more comfortable to ask questions by starting it off. In general, you create such a comfortable environment.",
+"Your love for Shawn!! Overall your such an easygoing person and a joy to be around.",
+"Your positivity!",
+"You can talk easily to anyone and you make anyone around you feel comfortable.",
+"bringing Ellie, your generosity, your smile, your cheerfulness--all these things make camp so much fun!",
+"that you are always so willing to talk to everyone",
+"How funny you are and your love for dogs!"
+"How inclusive you are when we do things!"
+"how nice and helpful you are. Immediately on the first day when we all came early, you were so easy to talk to and made me feel so welcome. You gave me your candy which was so nice and sweet. Things like that make me feel so happy and so welcomed in our community:)",
+"That you're very friendly and a very happy person. You're very easygoing and I like being your friend.",
+"I admire you persistence and how you always try to do the best whether it is your project or while your helping out others’ on theirs."
+]
+
+for (x of shoutouts) {
+  $("#shoutout-container").append("<p>" + x + "</p> <br>");
+}
